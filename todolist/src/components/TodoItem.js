f79ac89e8816eaacd513124aa6db4bec9faf1afa
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-04-18 15:28:34 +0100

    add change

--- todolist/src/components/TodoItem.js
+++ todolist/src/components/TodoItem.js
@@ -2,6 +2,24 @@ import React from "react"
 import { FaTrash } from "react-icons/fa"
 
 
+// class TodoItem extends React.Component {
+//   render() {
+    
+//     return <li className="item">
+//         <input
+//   type="checkbox"
+//   className="checkbox"
+//   checked={this.props.todo.completed}
+//   onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+//  />
+//         {this.props.todo.title}
+//         <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+//         <FaTrash />
+// </button>
+//         </li>
+//   }
+// }
+
 class TodoItem extends React.Component {
   render() {
     
